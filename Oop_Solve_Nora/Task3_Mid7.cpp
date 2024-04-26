f21f2b557cc09b120a90836721692063e15681b9
Nora <noraabood04@gmail.com>
2024-04-27 01:38:45 +0300

    Nora_solve

---
+++ Oop_Solve_Nora/Task3_Mid7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Complex {
+public:
+    double real;
+    double imag;
+
+    Complex(double r, double i) : real(r), imag(i) {}
+
+
+    friend Complex addComplex(const Complex& c1, const Complex& c2) {
+        return Complex(c1.real + c2.real, c1.imag + c2.imag);
+    }
+};
+
+int main() {
+    Complex first(3.0, 4.0);
+    Complex second(1.5, 2.5);
+
+    Complex third = addComplex(first, second);
+
+    cout << "Result: " << third.real << " + " << third.imag << "i" << endl;
+
+    return 0;
+}
+
