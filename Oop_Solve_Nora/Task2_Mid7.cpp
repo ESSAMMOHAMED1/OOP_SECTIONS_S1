e8767e6bc6cd12c911cb18198941d1d97c5e34e5
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-04-27 22:39:02 -0700

    just-format

--- Oop_Solve_Nora/Task2_Mid7.cpp
+++ Oop_Solve_Nora/Task2_Mid7.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 
 using namespace std;
-class frute{
+class frute
+{
 private:
     int Mango;
     int Apple;
     int price;
+
 public:
     void setMango(int m)
-     { Mango = m; }
+    {
+        Mango = m;
+    }
     void setApples(int a)
-     { Apple = a; }
+    {
+        Apple = a;
+    }
 
     int getMango() const
-    { return Mango; }
+    {
+        return Mango;
+    }
     int getApple() const
-    { return Apple; }
+    {
+        return Apple;
+    }
 
     int getPrice() const
-     { return Mango+Apple; }
-
+    {
+        return Mango + Apple;
+    }
 };
 
-int main() {
-     frute F1;
+int main()
+{
+    frute F1;
     cout << "Total price: " << F1.getPrice() << endl;
 
     return 0;
