759d7a8995c77d219d5eaa35036cbe8c7543cb29
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-04-27 22:40:47 -0700

    nora-task3-reviwe

--- Oop_Solve_Nora/Task3_Mid7.cpp
+++ Oop_Solve_Nora/Task3_Mid7.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
-
-class Complex {
+using namespace std;
+class Complex
+{
 public:
     double real;
     double imag;
 
     Complex(double r, double i) : real(r), imag(i) {}
 
-
-    friend Complex addComplex(const Complex& c1, const Complex& c2) {
+    friend Complex addComplex(const Complex &c1, const Complex &c2)
+    {
         return Complex(c1.real + c2.real, c1.imag + c2.imag);
     }
 };
 
-int main() {
+int main()
+{
     Complex first(3.0, 4.0);
     Complex second(1.5, 2.5);
 
@@ -23,4 +25,3 @@ int main() {
 
     return 0;
 }
-
